3e7d2f865158871123029134015b889cd79bcb75
jackson liu <nokiajackson@gmail.com>
2024-08-18 20:43:52 +0800

    後端 swagger 恢復 前端重修

--- secwagorevite.client/src/main.js
+++ secwagorevite.client/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/main.css'
+//import Login from './components/Login.vue';
 
-createApp(App).mount('#app')
+
+createApp(App)
+  .use(router)
+  .mount('#app')
